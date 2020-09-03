cb0ae7a42e5bee0d970303acb807829da926ccd9
root <root@6877eda396e5>
2020-09-04 03:22:06 +0900

    change static

--- complaintPrj/settings.py
+++ complaintPrj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'g)d-u^@b$qj_p73+psk1tt2^w(5m29u&r7!icg5y=u@_d*w#z-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -118,8 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+setting.py STATIC_URL = '/static/'
+PROJECT_ROOT_DIR = os.path.dirname(BASE_DIR)
+STATIC_ROOT = os.path.join(PROJECT_ROOT_DIR, 'dist', 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
